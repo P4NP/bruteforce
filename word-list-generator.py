8fd07f6b91df3911b443d76bf21f06d15a094772
P4NP <90383464+P4NP@users.noreply.github.com>
2021-09-10 19:24:36 +0545

    Update word-list-generator.py

--- word-list-generator.py
+++ word-list-generator.py
@@ -1,10 +1,16 @@
-import random
+
+	import random
 import colorama
 from colorama import Fore, Back, Style
-
+import sys
 import string
 from itertools import product
 from colorama import init
+
+if sys.version_info[0] != 3:
+    print('''\t--------------------------------------\n\t\tREQUIRED PYTHON 3.x\n\t\tinstall and try: python3 
+    fb.py\n\t--------------------------------------''')
+    sys.exit()
 init(autoreset=True)
 print(f"   {Fore.GREEN}  {Style.BRIGHT} WORDLIST GENERATOR")
 print("______________________________________")
@@ -14,7 +20,7 @@ print("______________________________________")
 print(f"{Style.BRIGHT}{Fore.RED}YouTube :P4NP ")
 print("______________________________________")
 
-num=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"]
+num=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"]
 
 A= input(f"{Style.BRIGHT}{Fore.GREEN}This is for educational process[Y/N]: ")
 if (A == "Y"):
@@ -27,17 +33,20 @@ elif (A == "n"):
 		print("use it in your own risk")		
 else:
 			print ("some thing went wrong")
+			sys.exit(0)
 	 	
 min_length = input("Enter the minimum length of  password: ")
 if min_length in num:
-	 	print("sucess fully inserted")
+	 	print("")
 else:
-	 		print("invalid or large num ")	
+	 		print("invalid or large num ")
+	 		sys.exit(0)	
 max_length= input("Enter the maximum length of password: ")
 if max_length in num:
-	print("Sucessfully inserted")
+	print("")
 else:
 	print("invalid or large num ")
+	sys.exit(0)
 	
 mini = int(min_length)	
 maxi=int(max_length)
@@ -54,5 +63,4 @@ for i in range (mini , maxi+1):
 			     		file_open.write("\n")
 			     		counter+=1
 			     		
-print("Password  list {} has been generated in password.txt" .format(counter))
-			     
+print("Password  list {} has been generated in password.txt" .format(counter))     
